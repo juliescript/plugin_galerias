3d0e7f3a19ed3522adfff76bc3bef4a5231ff1f0
Julie <julie@losmaquiladores.com>
2013-06-17 10:18:24 -0500

    Quitar console logs del javascript

--- js/functions.js
+++ js/functions.js
@@ -10,9 +10,9 @@ jQuery(document).ready(function($) {
     // $() will work as an alias for jQuery() inside of this function
     $('li.gallery-item').click(function(e){
     	e.preventDefault();
-    	console.log('Hago click');
+    	//console.log('Hago click');
     	var url = $(this).children().attr('href');
-    	console.log(url);
+    	//console.log(url);
     	window.open(url, '_blank', 'width='+screen.width+',height='+screen.height);
     });
 });
